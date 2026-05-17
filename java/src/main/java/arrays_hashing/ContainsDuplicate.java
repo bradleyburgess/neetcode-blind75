b252ca4600fb7b217448298e986f021c5fe41b1f
Bradley Burgess <bradleyburgess@gmail.com>
2026-05-17 16:34:26 +0200

    Add sort solution

--- java/src/main/java/arrays_hashing/ContainsDuplicate.java
+++ java/src/main/java/arrays_hashing/ContainsDuplicate.java
@@ -1,5 +1,6 @@
 package arrays_hashing;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class ContainsDuplicate {
@@ -17,6 +18,15 @@ public class ContainsDuplicate {
         for (int num : nums) {
             if (hashMap.containsKey(num)) return true;
             hashMap.put(num, true);
+        }
+        return false;
+    }
+
+    public boolean solveSort(int[] nums) {
+        var _nums = nums.clone();
+        Arrays.sort(_nums);
+        for (int i = 0; i < _nums.length - 1; i++) {
+            if (_nums[i] == _nums[i + 1]) return true;
         }
         return false;
     }
